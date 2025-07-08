57592fd71dc7d36110222733252b839a32d3ef28
Azka Andya <azka.safira@mitra.jasamarga.co.id>
2025-07-08 09:36:35 +0700

    feat: add Projects data and associated images for Alila Hotel and Imensa Apartment

---
+++ app/projects.ts
@@ -0,0 +1,31 @@
+export const Projects = [
+    {
+        id: 1,
+        title: "Alila Hotel With Budi Lim Arsitek",  
+        image: "/img/project103-alila3.jpg",
+        description: "PROJECT : ALILA HOTEL, DESIGN FIRM : BUDI LIM ARCHITECT, ARCHITECT : YUDITIA",
+        images: [
+            "/img/project103-alila2.jpg",
+            "/img/project103-alila3.jpg",
+            "/img/project103-alila4.jpg"
+        ],
+        location: "JAKARTA, INDONESIA",
+        category: "Architecture",
+        people: "Yuditia"
+    },
+     {
+        id: 2,
+        title: "IMENSA APARTMENT with HB DESIGN, PTE LTD",  
+        image: "/img/project20.jpg",
+        description: "PROJECT : IMENSA APARTMENT, LOCATION : THANE, INDIA, DESIGN FIRM : HB DESIGN, PTE LTD, ARCHITECT : YUDITIA",
+        images: [
+            "/img/project19.jpg",
+            "/img/project20.jpg",
+            "/img/project21.jpg",
+            "/img/project22.jpg",
+        ],
+        location: "India",
+        category: "Architecture",
+        people: "Yuditia"
+    }
+]
